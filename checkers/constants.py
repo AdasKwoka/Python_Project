7d58ac9ffbaf763c9139deb154a6145f04bc9995
Adam Kwoka <adam.kwoka99@gmail.com>
2021-06-01 11:27:42 +0200

    Add constants file, Add create win in main.py

---
+++ checkers/constants.py
@@ -0,0 +1,10 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+FPS = 60
+
+# Tworzenie okna gry
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Warcaby')
+
+
+def main():
+    run = True
+    # ustawienie odswiezania na 60FPS dzieki czemu bedziemy mieli plynna rozgrywke
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
+
+
+main()
