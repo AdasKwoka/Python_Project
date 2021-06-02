2514bee6922e901be5d749310799688222601b3f
Adam Kwoka <adam.kwoka99@gmail.com>
2021-06-02 08:14:24 +0200

    Add game.py, move to pieces

--- checkers/game.py
+++ checkers/game.py
@@ -1,6 +1,6 @@
 import pygame
 from checkers.board import Board
-from .constants import WHITE, BLACK
+from .constants import WHITE, BLACK, GREEN, SQUARE_SIZE
 
 
 class Game:
@@ -10,13 +10,17 @@ class Game:
 
     def update(self):
         self.board.draw(self.win)
+        self.draw_valid_moves(self.valid_moves)
         pygame.display.update()
 
     def _init(self):
         self.selected = None
         self.board = Board()
-        self.turn = WHITE
+        self.turn = BLACK
         self.valid_moves = {}
+
+    def winner(self):
+        return self.board.winner()
 
     def reset(self):
         self._init()
@@ -27,12 +31,12 @@ class Game:
             if not result:
                 self.selected = None
                 self.select(row, col)
-        else:
-            piece = self.board.get_piece(row, col)
-            if piece != 0 and piece.color == self.turn:
-                self.selected = piece
-                self.valid_moves = self.board.get_valid_moves(piece)
-                return True
+
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
 
         return False
 
@@ -40,11 +44,22 @@ class Game:
         piece = self.board.get_piece(row, col)
         if self.selected and piece == 0 and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
+            self.change_turn()
         else:
             return False
         return True
 
+    def draw_valid_moves(self, moves):
+        for move in moves:
+            row, col = move
+            pygame.draw.circle(self.win, GREEN, (col * SQUARE_SIZE +
+                               SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2), 15)
+
     def change_turn(self):
+        self.valid_moves = []
         if self.turn == WHITE:
             self.turn = BLACK
         else:
